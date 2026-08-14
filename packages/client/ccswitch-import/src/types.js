0f0e2ffa57f3e101baf156b5199eb00adb08d395
陈浩 <452218405@qq.com>
2026-08-14 14:29:36 +0800

    chore(custom): 移除误入 src 的编译产物

--- packages/client/ccswitch-import/src/types.js
+++
@@ -1,3 +0,0 @@
-/** Wire vocabulary for the CCSwitch import Remote and its settings UI. */
-export {};
-//# sourceMappingURL=types.js.map
--- packages/client/prompt-deepen/src/types.js
+++
@@ -1,3 +0,0 @@
-/** Wire vocabulary for the Prompt Deepen Remote. */
-export {};
-//# sourceMappingURL=types.js.map
